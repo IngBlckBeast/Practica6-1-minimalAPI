afc57b2f1180f2c28b10d9f74cf67f1a143a36b2
BlckBeast <danielmoralesv95@gmail.com>
2025-11-23 13:50:45 -0600

    feat: se anexo docker-compose.yml

--- 6-1/Models/user.cs
+++ 6-1/Models/user.cs
@@ -6,6 +6,10 @@ public class User
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    
+    // --- NUEVO: La contraseña legible ---
+    public string Password { get; set; } = string.Empty; 
+    
     public string PasswordHash { get; set; } = string.Empty;
-    public string RoleName { get; set; } = string.Empty; // Aquí guardaremos el nombre del rol (Admin/User)
+    public string RoleName { get; set; } = string.Empty;
 }
--- 6-1/Repositories/UserRepository.cs
+++ 6-1/Repositories/UserRepository.cs
@@ -10,14 +10,13 @@ public class UserRepository : IUserRepository
     {
         _connectionString = configuration.GetConnectionString("DefaultConnection")!;
     }
-
     public async Task<User?> GetUserByEmailAsync(string email)
     {
         using var connection = new SqlConnection(_connectionString);
         
-        // Hacemos JOIN para traer el nombre del Rol directamente
+        // AGREGAMOS u.Password AL SELECT
         var sql = @"
-            SELECT u.Id, u.FirstName, u.LastName, u.Email, u.PasswordHash, r.Name as RoleName
+            SELECT u.Id, u.FirstName, u.LastName, u.Email, u.Password, u.PasswordHash, r.Name as RoleName
             FROM Users u
             INNER JOIN Roles r ON u.RoleId = r.Id
             WHERE u.Email = @Email";
